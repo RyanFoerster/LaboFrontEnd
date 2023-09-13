a1c15046e2093413a369307505ceef396cdcf758
Loïc Vanorshoven <lovanorshoven@gmail.com>
2023-09-13 15:28:05 +0200

    active-account-dev/recruiter ok

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -22,8 +22,12 @@ export class AuthService {
         this._token$.subscribe(console.log)
     }
 
-    registerDev(user:DevRegister): Observable<User>{
+    registerDev(user: DevRegister): Observable<User> {
         return this._httpClient.post<User>(`${environments.apiUrl}/auth/dev-register`, user)
+    }
+
+    activeDev(confirmationToken : string): Observable<User> {
+        return this._httpClient.patch<User>(`${environments.apiUrl}/dev/${confirmationToken}`, confirmationToken)
     }
 
     registerRecruiter(user: RecruiterRegister): Observable<User> {
@@ -31,19 +35,23 @@ export class AuthService {
         return this._httpClient.post<User>(`${environments.apiUrl}/auth/recruiter-register`, user)
     }
 
-    login(username: string, password: string): Observable<Token>{
+    activeRecruiter(confirmationToken: string): Observable<User> {
+        return this._httpClient.patch<User>(`${environments.apiUrl}/recruiter/${confirmationToken}`, confirmationToken)
+    }
+
+    login(username: string, password: string): Observable<Token> {
         return this._httpClient.post<Token>(`${environments.apiUrl}/auth/login`, {
             username,
             password
         }).pipe(
-            tap(response => this.addToSession(response) )
+            tap(response => this.addToSession(response))
         )
     }
 
-    logout(){
+
+    logout() {
         this.removeFromSession()
     }
-
 
     private addToSession(token: Token) {
         localStorage.setItem('token', JSON.stringify(token))
@@ -51,7 +59,7 @@ export class AuthService {
         this._token$.next(token)
     }
 
-    private removeFromSession(){
+    private removeFromSession() {
         localStorage.removeItem('token')
 
         this._token$.next(null)
@@ -61,23 +69,23 @@ export class AuthService {
         return this._token$.getValue()
     }
 
-    get token$(): Observable<Token | null>{
+    get token$(): Observable<Token | null> {
         return this._token$.asObservable()
     }
 
-    get user(){
+    get user() {
         return this.token?.userDTO;
     }
 
-    get user$(): Observable<User | undefined>{
+    get user$(): Observable<User | undefined> {
         return this._token$.pipe(
-            map( token => token?.userDTO )
+            map(token => token?.userDTO)
         )
     }
 
     get isLogged$() {
         return this._token$.pipe(
-            map( token => !!token )
+            map(token => !!token)
         )
     }
 }
