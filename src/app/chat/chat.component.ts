a64fd11d031170822f63745c71c16e0815b18201
RyanFoerster <73285979+RyanFoerster@users.noreply.github.com>
2023-09-14 20:22:17 +0200

    Messagerie toujopurs pas

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -2,13 +2,19 @@ import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from "@angular/forms"
 import {Client} from '@stomp/stompjs';
+import {User} from "../shared/models/User";
+import {AuthService} from "../shared/services/auth.service";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
     selector: 'app-chat',
     standalone: true,
     imports: [
         CommonModule,
-        FormsModule
+        FormsModule,
+        MatInputModule,
+        MatButtonModule
     ],
     templateUrl: './chat.component.html',
     styleUrls: ['./chat.component.scss']
@@ -17,23 +23,26 @@ export class ChatComponent implements OnInit{
 
     private stompClient!: Client;
     text: string[] = []
-
-    constructor() { }
+    userConnected!: User | undefined
+    messageToSend: string = ""
+    sessionId: string = ""
+    constructor(private _authService: AuthService) { }
 
     ngOnInit() {
         this.stompClient = new Client({
-            brokerURL: 'ws://localhost:8080/chat'
+            brokerURL: 'ws://localhost:8080/hello'
         });
 
-
+        this.userConnected = this._authService.user
     }
 
     connect() {
         this.stompClient.activate();
         this.stompClient.onConnect = (frame) => {
-            console.log('Connected: ' + frame);
-            this.stompClient.subscribe('/topic/messages', (message) => {
+            console.log("connected" + frame)
+            this.stompClient.subscribe(`/user/${this.userConnected?.username}/queue/private-reply`, (message) => {
                 this.text.push(message.body)
+                console.log(this.text.length)
             });
         };
 
@@ -52,19 +61,17 @@ export class ChatComponent implements OnInit{
         console.log("Disconnected");
     }
 
-    sendName() {
+    sendMessage(message: string) {
         this.stompClient.publish({
-            destination: "/app/message",
+            destination: "/app/private-message",
             body: JSON.stringify({
-                message: "Test",
-                receptorId: 2,
-                emitterId: 1
+                emitter: this.userConnected?.username,
+                receptor: "string2",
+                message: message
             })
         });
-    }
 
-    showGreeting(message: string) {
-
+        this.messageToSend = "";
     }
 
 }
