f60c763cc6cd278bdbe89db71442098f5b38dfdd
loic vanorshoven <loic.vanorshoven@hotmail.com>
2023-09-19 08:14:09 +0200

    update mdp rec & dev ok

---
+++ src/app/shared/models/DevInfo.ts
@@ -0,0 +1,21 @@
+import {Address} from "./Address";
+import {TechnologyBackEnd} from "./enums/TechnologyBackEnd";
+import {TechnologyFrontEnd} from "./enums/TechnologyFrontEnd";
+
+export interface DevInfo {
+    id: number;
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    pseudo: string;
+    gitHub: string;
+    linkedIn: string;
+    cv: string;
+    technologyBackEnds: TechnologyBackEnd[];
+    technologyFrontEnds: TechnologyFrontEnd[];
+    role: string[];
+    address: Address;
+
+
+}
